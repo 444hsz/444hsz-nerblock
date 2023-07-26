fc2c21ffdddad17297f52bf29c4339c7042a1bba
nerblock <nerblock420@gmail.com>
2023-07-26 07:45:01 +0200

    removed unsafe innerhtml +  prettier

--- nerblock-pageblocked.js
+++ nerblock-pageblocked.js
@@ -1,17 +1,20 @@
 if (typeof chrome.runtime === "undefined") chrome = browser;
 
 function initPage() {
-    var urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.has('url')) {
-        var url = urlParams.get('url').replace(/^http(s?):\/\//, "").replace(/\/$/, "");
-        var blockedUrlEl = document.querySelector("#blockedUrl");
-        document.title = url;
-        blockedUrlEl.innerHTML = url;
-    }
+  var urlParams = new URLSearchParams(window.location.search);
+  if (urlParams.has("url")) {
+    var url = urlParams
+      .get("url")
+      .replace(/^http(s?):\/\//, "")
+      .replace(/\/$/, "");
+    var blockedUrlEl = document.querySelector("#blockedUrl");
+    document.title = url;
+    blockedUrlEl.textContent = url;
+  }
 }
 
 if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", initPage);
+  document.addEventListener("DOMContentLoaded", initPage);
 } else {
-    initPage();
+  initPage();
 }
